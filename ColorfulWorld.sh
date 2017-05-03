301cadbfec348651798dcb497e816dc23fca9ca3
DevOooops <terrancezhu@yahoo.com>
2017-05-03 21:39:02 +1000

    Update ColorfulWorld.sh

--- ColorfulWorld.sh
+++ ColorfulWorld.sh
@@ -1,37 +1,29 @@
 #!/bin/bash
 #Show you some color
 
-clear
-
-echo -e "33[1m Colorful World"
-
+clear 
+echo -e "33[1m Hello World"
+# bold effect
 echo -e "33[5m Blink"
-
-echo -e "33[0m Colorful World"
-
-echo -e "33[31m Colorful World"
-
-echo -e "33[32m Colorful World"
-
-echo -e "33[33m Colorful World"
-
-echo -e "33[34m Colorful World"
-
-echo -e "33[35m Colorful World"
-
-echo -e "33[36m Colorful World"
-
+# blink effect
+echo -e "33[0m Hello World"
+# back to normal
+echo -e "33[31m Hello World"
+# Red color
+echo -e "33[32m Hello World"
+# Green color
+echo -e "33[33m Hello World"
+# See remaining on screen
+echo -e "33[34m Hello World"
+echo -e "33[35m Hello World"
+echo -e "33[36m Hello World"
 echo -e -n "33[0m"
-
-echo -e "33[41m Colorful World"
-
-echo -e "33[42m Colorful World"
-
-echo -e "33[44m Colorful World"
-
-echo -e "33[45m Colorful World"
-
-echo -e "33[46m Colorful World"
-
-echo -e "33[0m Colorful World"
+# back to normal
+echo -e "33[41m Hello World"
+echo -e "33[42m Hello World"
+echo -e "33[43m Hello World"
+echo -e "33[44m Hello World"
+echo -e "33[45m Hello World"
+echo -e "33[46m Hello World"
+echo -e "33[0m Hello World"
 
