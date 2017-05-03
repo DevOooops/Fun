5ac957f3625f482d8f96d5eb6fff19d97fe494c5
DevOooops <terrancezhu@yahoo.com>
2017-05-03 21:40:24 +1000

    Update ColorfulWorld.sh

--- ColorfulWorld.sh
+++ ColorfulWorld.sh
@@ -2,28 +2,28 @@
 #Show you some color
 
 clear 
-echo -e "33[1m Hello World"
+echo -e "33[1m Color"
 # bold effect
 echo -e "33[5m Blink"
 # blink effect
-echo -e "33[0m Hello World"
+echo -e "33[0m Color"
 # back to normal
-echo -e "33[31m Hello World"
+echo -e "33[31m Color"
 # Red color
-echo -e "33[32m Hello World"
+echo -e "33[32m Color"
 # Green color
-echo -e "33[33m Hello World"
+echo -e "33[33m Color"
 # See remaining on screen
-echo -e "33[34m Hello World"
-echo -e "33[35m Hello World"
-echo -e "33[36m Hello World"
+echo -e "33[34m Color"
+echo -e "33[35m Color"
+echo -e "33[36m Color"
 echo -e -n "33[0m"
 # back to normal
-echo -e "33[41m Hello World"
-echo -e "33[42m Hello World"
-echo -e "33[43m Hello World"
-echo -e "33[44m Hello World"
-echo -e "33[45m Hello World"
-echo -e "33[46m Hello World"
-echo -e "33[0m Hello World"
+echo -e "33[41m Color"
+echo -e "33[42m Color"
+echo -e "33[43m Color"
+echo -e "33[44m Color"
+echo -e "33[45m Color"
+echo -e "33[46m Color"
+echo -e "33[0m Color"
 
